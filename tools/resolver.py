c00aa03df2fbb4c61eb9e2d0019eea2e0a1908cf
Maxime Daniel <root@maxux.net>
2023-04-04 01:20:15 +0200

    resolver: add whois handler

--- tools/resolver.py
+++ tools/resolver.py
@@ -1,6 +1,7 @@
 import socket
 import redis
 import time
+from ipwhois import IPWhois
 
 r = redis.Redis()
 pubsub = r.pubsub()
@@ -16,13 +17,28 @@ while True:
 
         print("[+] resolving: %s" % address)
 
+        found = False
+
         try:
             host = socket.gethostbyaddr(address)
             r.setex("resolver-%s" % address, 30, host[0])
             print("[+] %s -> %s" % (address, host[0]))
+            found = True
 
         except Exception as e:
             print("[-] could not resolve: %s (%s)" % (address, e))
-            r.setex("resolver-%s" % address, 30, "(unknown)")
+            found = False
+
+        if not found:
+            try:
+                resolv = IPWhois(address)
+                w = resolv.lookup_whois()
+                description = w['nets'][0]['description']
+
+                print("[+] %s -> %s" % (address, description))
+                r.setex("resolver-%s" % address, 30, description)
+
+            except Exception as e:
+                print(e)
 
     time.sleep(0.1)
