9d40f174a968be4dd0e1bee658ca9d51ea390730
Maxime Daniel <root@maxux.net>
2023-04-04 01:13:49 +0200

    db: remove old schema

--- db/schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE sensors (id varchar(32), timestamp integer, value float, primary key (id, timestamp));
-CREATE TABLE IF NOT EXISTS "power" (timestamp int, value float, phase int, primary key (timestamp, phase));
-CREATE INDEX idxsensors on sensors(id, timestamp, value);
-CREATE INDEX idphase on power (phase);
-CREATE TABLE dht (id varchar(6), timestamp integer, temp float, hum float, primary key (id, timestamp));
