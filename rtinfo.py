6a9c6e1f84a0016fc2e83fcaf8b3a22ce6961ddd
Maxime Daniel <root@maxux.net>
2026-03-31 09:03:15 +0200

    rtinfo: dirty hack to handle keep-alive http request

--- rtinfo.py
+++ rtinfo.py
@@ -1,4 +1,6 @@
-import requests
+# import requests
+import socket
+import json
 from config import dashconfig
 from dashboard import DashboardSlave
 
@@ -8,12 +10,43 @@ while True:
     print("[+] rtinfo: fetching")
 
     try:
+        """
         response = requests.get(dashconfig['rtinfo-endpoint'], timeout=2)
         slave.set(response.json())
+        """
 
-        print("[+] rtinfo: %d hosts found" % len(slave.payload['rtinfo']))
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((dashconfig['rtinfo-host'], dashconfig['rtinfo-port']))
 
-        slave.publish()
+            while True:
+                s.sendall(f"GET {dashconfig['rtinfo-uri']} X\n".encode("utf-8"))
+                header = True
+                buffer = b""
+
+                while header:
+                    buffer += s.recv(128)
+                    if b"\r\n\r\n" in buffer:
+                        header = False
+
+                headers = buffer.decode("utf-8").split("\r\n")
+                expected = 0
+
+                for x in headers:
+                    if x.lower().startswith("content-length: "):
+                        expected = int(x[16:])
+
+                leftover = headers[-1].encode("utf-8")
+                expected -= len(leftover)
+
+                payload = leftover + s.recv(expected)
+                slave.set(json.loads(payload))
+
+                hosts = len(slave.payload['rtinfo'])
+                size = len(payload)
+
+                print(f"[+] rtinfo: {hosts} hosts found, {size} bytes")
+                slave.publish()
+                slave.sleep(1)
 
     except Exception as e:
         print(e)
